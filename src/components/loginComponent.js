8a44560aae887a6b578ec89f301715d5c402b824
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-02 19:05:38 +0100

    responses, just to know

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -7,23 +7,43 @@ class LoginComponent extends React.Component{
         this.state = {
             username: '',
             password: '',
+            isValid: undefined,
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInputs = this.handleInputs.bind(this);
+        this.createLoginResponse = this.createLoginResponse.bind(this);
     }
 
     handleSubmit(e){
         e.preventDefault();
+
+        const isCorrect = this.state.username === 'test' && this.state.password === 'test';
+
+        this.setState({ isValid: isCorrect });
     }
 
     handleInputs(e){
         this.setState({[e.target.name]: e.target.value});
     }
 
+    createLoginResponse(){
+        if ( this.state.isValid === undefined ){
+            return '';
+        } else if( this.state.isValid ) {
+            return 'Logged in';
+        } else {
+            return 'Wrong credentials';
+        }
+    }
+
     render(){
         return (
-            <div id="login__container">
+            <div class="login__container">
+                <h2 class="login__container__response">
+                    {this.createLoginResponse()}
+                </h2>
+
                 <form onSubmit={this.handleSubmit}>
                     <input type="text" name="username" value={this.state.username} onChange={this.handleInputs} />
                     <input type="password" name="password" value={this.state.password} onChange={this.handleInputs}/>
