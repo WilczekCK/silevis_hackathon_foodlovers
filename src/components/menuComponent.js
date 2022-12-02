498823cd3aabd4e3bde212d887ce24356e4f710d
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-02 19:30:51 +0100

    created navbar component

---
+++ src/components/menuComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MenuComponent extends React.Component{
+    constructor(){
+        super();
+    }    
+
+    render(){
+        return (
+            <div className="navbar__container">
+                
+            </div>
+        )
+    }
+}
