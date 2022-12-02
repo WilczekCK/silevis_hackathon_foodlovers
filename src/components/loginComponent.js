1e5390579a1a148b2abfc470bcc663d75f4a1bef
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-02 18:54:47 +0100

    state of inputs, on change applied

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -4,21 +4,31 @@ class LoginComponent extends React.Component{
     constructor(){
         super();
 
+        this.state = {
+            username: '',
+            password: '',
+        }
+
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputs = this.handleInputs.bind(this);
     }
 
     handleSubmit(e){
         e.preventDefault();
     }
 
+    handleInputs(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render(){
         return (
             <div id="login__container">
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="username" />
-                    <input type="password" name="password" />
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleInputs} />
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleInputs}/>
 
-                    <input type="submit" />
+                    <input type="submit" value="Login"/>
                 </form>
             </div>
         )
