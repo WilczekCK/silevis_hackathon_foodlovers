a9db4538c8886daf04b9cb9a1eca9c98e2986831
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-02 22:47:36 +0100

    imported leaflet and created new view

---
+++ src/views/Events.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Events extends React.Component{
+    constructor(){
+        super();
+    }
+
+
+    render(){
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
+
+export default Events;
---
+++ src/views/Map.js
@@ -0,0 +1,19 @@
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import React from "react";
+
+class Map extends React.Component{
+    constructor(){
+        super();
+    }
+
+
+    render(){
+        return (
+            <>
+                
+            </>
+        )
+    }
+}
+
+export default Map;
