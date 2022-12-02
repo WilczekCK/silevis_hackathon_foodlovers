f63317e7bff642de15cf138c5ba581213e2e6c63
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-03 00:52:11 +0100

    front page css ended

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -11,7 +11,19 @@ class Homepage extends React.Component{
             <>
                 <h1>Witaj<br/> w BookSport!</h1>
                 <p>Wyszukaj miejsce, znajdź lub utwórz druzynę i zagrajcie razem w piłkę nozną</p>
-                <hr/>
+               
+                <div class="homepage__container__bottomAlign">
+                    <div class="homepage__container_bottomAlign--horizontal">
+                        <button>Zaloguj sie</button>
+                        <button>Zarejestruj sie</button>
+                    </div>
+
+                    <a>Moja druzyna</a>
+                    <a>Najblizsze wydarzenia</a>
+
+                    <hr/>
+                </div>
+                
             </>
         )
     }
