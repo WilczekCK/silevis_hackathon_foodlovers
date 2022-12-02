67a98561fd4b5e2d5354c6ce1a2b8bda600f8a8f
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-02 23:19:11 +0100

    multiple events goes brr brr

--- src/views/Map.js
+++ src/views/Map.js
@@ -14,6 +14,13 @@ L.Marker.prototype.options.icon = DefaultIcon;
 class Map extends React.Component{
     constructor(){
         super();
+
+        this.listOfEvents = [
+            [50.86942, 20.61824],
+            [50.86884, 20.63111],
+            [50.87371, 20.63187],
+            [50.86979, 20.64189]
+        ]
     }
 
 
@@ -25,11 +32,15 @@ class Map extends React.Component{
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[50.86942, 20.61824]}>
-                    <Popup>
-                    Zajebscie :D
-                    </Popup>
-                </Marker>
+
+                {this.listOfEvents.map((mapPos, index) => 
+                    <Marker key={index} position={mapPos}>
+                        <Popup>
+                        Zajebscie :D
+                        </Popup>
+                    </Marker>
+                )}
+
                 </MapContainer>
             </>
         )
