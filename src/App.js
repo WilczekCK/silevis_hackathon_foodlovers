afe2763e055f95f25923c7f78f85de19c51d688b
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-02 22:56:44 +0100

    css/images for leaflet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import React from "react";
 import CookieInstation from "./controllers/cookieController";
 import MenuComponent from './components/menuComponent'
+import Map from './views/Map'
 import './App.css';
+import './leaflet/leaflet.css';
+
 
 class App extends React.Component{
   constructor(){
@@ -28,7 +31,9 @@ class App extends React.Component{
   render(){
     return (
       <div className="App">
-          <div className="App__map"></div>
+          <div className="App__map">
+            <Map/>
+          </div>
           <div className="App__menu">
             <MenuComponent 
               cookieInfo={this.state.cookieInfo}
--- src/views/Map.js
+++ src/views/Map.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import React from "react";
 
 class Map extends React.Component{
@@ -10,7 +10,17 @@ class Map extends React.Component{
     render(){
         return (
             <>
-                
+                <MapContainer center={[50.8708,20.6478]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+                </MapContainer>
             </>
         )
     }
